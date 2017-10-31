506e2c65438d56e1cda6921d4015cc783ec43a51
Longo-Zak-ACSC <longo.zak.acsc@gmail.com>
2017-10-31 14:46:28 -0700

    prepare setUser factory stub

--- js/setUser.js
+++ js/setUser.js
@@ -1,18 +1,14 @@
 // set users
 app.factory("User", function ($http) {
-    // Define the DribbblePlayer function
+    // Define the setUser function
     var TodoUser = function (user) {
         // Define the initialize function
         this.initialize = function () {
-            // Fetch the player from Dribbble
-            var url = 'http://api.dribbble.com/players/' + player + '?callback=JSON_CALLBACK';
-            var playerData = $http.jsonp(url);
+            var url = '';
+            var userData = $http.jsonp(url);
             var self = this;
 
-            // When our $http promise resolves
-            // Use angular.extend to extend 'this'
-            // with the properties of the response
-            playerData.then(function (response) {
+            userData.then(function (response) {
                 angular.extend(self, response.data);
             });
         };
@@ -22,5 +18,5 @@ app.factory("User", function ($http) {
     };
 
     // Return a reference to the function
-    return (DribbblePlayer);
+    return ();
 });
